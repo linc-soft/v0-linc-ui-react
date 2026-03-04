b5f115362deef5ab3113dce77655e834628581bc
宋秀林 <songxiulin@linc-soft.com>
2026-03-04 23:14:35 +0800

    chore: 更新配置文件并添加 TextInput 的 Storybook 故事
    - 将 coverage 目录添加到 .gitignore 和 .prettierignore
    - 更新 ESLint 配置以忽略 storybook-static 目录
    - 简化 Storybook 配置中的 stories 路径
    - 修复 TextInput 组件中的代码格式问题
    - 将 TextInput 的 Storybook 故事迁移到标准 stories 目录

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/stories/**/*.stories.@(ts|tsx)',
-    '../src/components/linc-ui/**/*.stories.@(ts|tsx)',
-  ],
+  stories: ['../src/stories/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-vite',
